1ee03ddbf4ffa0e4166583aa3f34a6a0b4a8a663
yusuferenn <yusuferenozcan@gmail.com>
2023-05-30 22:28:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const key = 'a9f7a613d274e31b53f83c456ae19487';
+
+const url = 'https://api.openweathermap.org/data/2.5/'
+
+const setQuery = (e) => {
+    if(e.keyCode =='13')
+    getResult(searchBar.value)
+}
+
+const getResult = (cityName) => {
+    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
+    fetch(query)
+    .then(weather => {
+        return weather.json()
+    })
+    .then(displayResult)
+}
+
+const displayResult = (result) => {
+    let city = document.querySelector('.city')
+    city.innerHTML = `${result.name}, ${result.sys.country}`
+
+    let temp = document.querySelector('.temp')
+    temp.innerHTML = `${Math.round(result.main.temp)}°C`
+
+    let desc = document.querySelector('.desc')
+    desc.innerHTML = result.weather[0].description
+
+    let minmax = document.querySelector('.minmax')
+    minmax.innerHTML = `${Math.round(result.main.temp_min)}°C / 
+    ${Math.round(result.main.temp_max)}°C`
+}
+
+const searchBar = document.getElementById('searchBar') ;
+searchBar.addEventListener('keypress', setQuery)
